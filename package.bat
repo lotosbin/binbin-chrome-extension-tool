19000e350eabda6333779a5b08df95ba7608a2b1
lotosbin <lotosbin@gmail.com>
2012-11-13 14:09:15 +0800

    package parent directory

--- package.bat
+++ package.bat
@@ -1,2 +1,2 @@
 del *.zip
-zip -r package.zip ./ -i "*.json" -i "*.js" -i "*.html" -i "*.css" -i "*.png" -i "*.gif"
+zip -r package.zip ../ -i "*.json" -i "*.js" -i "*.html" -i "*.css" -i "*.png" -i "*.gif"
